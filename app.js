d06e51cb6745a67a55ae748e3a35551d2623a675
user <uokorocha@icitsolutions.com>
2023-06-22 16:40:22 +0100

    fixing cors

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ require("./services/auth")
 
 const app = express();
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+  });
 app.use(passport.initialize());
 
 
