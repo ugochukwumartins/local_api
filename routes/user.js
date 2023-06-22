c284d09738c02334940c566455c0c1b127630163
user <uokorocha@icitsolutions.com>
2023-06-22 13:49:11 +0100

    bug fix

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,35 @@ const passport = require("passport")
 const userController = require('../controller/userController');
 const localRouter = express.Router();
 
+/**
+ * @swagger
+ *  components:
+ *      schema:
+ *          User:
+ *              type: object
+ *              properties:
+ *                  email: 
+ *                      type: string
+ *                  password:
+ *                      type: string  
+ *                  user_name:
+ *                      type: string
+ */
 
 
-
-
+// /**
+//  * @swagger
+//  * /signUp:
+//  *   post:
+//  *     summary: Get all state in Nigeria by region after registration
+//  *     description: sign up to be able to get your api keys 
+//  *     requestBody:
+//  *            required: true
+//  *            content:
+//  *              application/json:
+//  *                schema:
+//  *                   $ref: '#components/schema/User'
+//  */
 localRouter.post("/signUp",userController.signup );
 localRouter.post("/login",passport.authenticate('headerapikey', { session: false }), userController.login);
 
